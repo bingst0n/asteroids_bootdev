7abc3ef53898ff0566638b5a468a06cd167c07d0
Harrison Green <harrisonrgreen@gmail.com>
2026-06-22 13:16:45 -0400

    setup movement and asteroids

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt: float = 0.0
+
 
     while True:
 
@@ -19,6 +37,17 @@ def main():
                 return
 
         screen.fill("black")
+        for thing in drawable:
+            thing.draw(screen)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
